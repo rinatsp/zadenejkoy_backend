1885f6bd4490c60a483a061345cff6dd729aec1b
idanyaqq <danucemo994@gmail.com>
2018-11-10 20:10:03 +0300

    edited ATM

--- src/main/java/com/vk/hack/atmmap/controller/TestController.java
+++ src/main/java/com/vk/hack/atmmap/controller/TestController.java
@@ -2,11 +2,18 @@ package com.vk.hack.atmmap.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mastercard.api.core.ApiConfig;
+import com.mastercard.api.core.exception.ApiException;
+import com.mastercard.api.core.model.RequestMap;
+import com.mastercard.api.core.security.oauth.OAuthAuthentication;
+import com.mastercard.api.locations.ATMLocations;
 import com.vk.hack.atmmap.model.ATM;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
--- src/main/java/com/vk/hack/atmmap/model/ATM.java
+++ src/main/java/com/vk/hack/atmmap/model/ATM.java
@@ -1,19 +1,33 @@
 package com.vk.hack.atmmap.model;
 
 public class ATM {
+    private long id;
+    private String address;
     private String city;
     private double lon;
     private double lat;
+    private String bank;
     private String name;
+    private Type type;
 
     public ATM() {
     }
 
-    public ATM(String city, double lon, double lat, String name) {
+    public enum Type{
+        ATM,
+        TERMINAL,
+        CASHBOX
+    }
+
+    public ATM(long id, String address, String city, double lon, double lat, String bank, String name, Type type) {
+        this.id = id;
+        this.address = address;
         this.city = city;
         this.lon = lon;
         this.lat = lat;
+        this.bank = bank;
         this.name = name;
+        this.type = type;
     }
 
     public String getCity() {
@@ -22,6 +36,15 @@ public class ATM {
 
     public void setCity(String city) {
         this.city = city;
+    }
+
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
     }
 
     public double getLon() {
@@ -38,6 +61,30 @@ public class ATM {
 
     public void setLat(double lat) {
         this.lat = lat;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getName() {
